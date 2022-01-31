f3643d29dbb74fc7a8cf101639919c95d605e8b8
Manish <manishkumar0156@gmail.com>
2022-01-31 12:16:30 +0530

    [Manish] Add: check option of Snake and ladder

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadderGame.cs
@@ -16,5 +16,23 @@ namespace SnakeAndLadder
             int dice = random.Next(1, 7);
             return dice;
         }
+        public void Play()
+        {
+            const int NOPLAY = 0, LADDER = 1, SNAKE = 2;
+            int option = random.Next(1, 3);
+            int diceoutcome = DiceRoll();
+            switch (option)
+            {
+                case LADDER:
+                    playerPosition += diceoutcome;
+                    break;
+                case SNAKE:
+                    playerPosition -= diceoutcome;
+                    break;
+
+                case NOPLAY:
+                    break;
+            }
+        }
     }
 }
