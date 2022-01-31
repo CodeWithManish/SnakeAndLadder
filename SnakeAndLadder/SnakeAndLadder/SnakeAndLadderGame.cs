6c8e3b0034c06b3500cbdecf501d95b56ea01c16
Manish <manishkumar0156@gmail.com>
2022-01-31 12:30:19 +0530

    [Manish] Add: checking the dice was played

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadderGame.cs
@@ -8,18 +8,19 @@ namespace SnakeAndLadder
 {
     public class SnakeAndLadderGame
     {
-        int playerPosition = 0;
+        int playerPosition = 0; int counter = 0;
 
         Random random = new Random();
         public int DiceRoll()
         {
             int dice = random.Next(1, 7);
+            counter++;
             return dice;
         }
         public void Play()
         {
             const int NOPLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
-            int option = random.Next(0, 3);
+            int option = random.Next(1, 3);
             int diceoutcome = DiceRoll();
 
             while (playerPosition < WINNING_POSITION)
@@ -42,7 +43,9 @@ namespace SnakeAndLadder
                     case NOPLAY:
                         break;
                 }
+                Console.WriteLine("player position are:{0}", playerPosition);
             }
+            Console.WriteLine("the total number of time the Dice was played {0}", counter);
 
         }
     }
